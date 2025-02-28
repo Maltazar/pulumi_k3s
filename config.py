7d3547626f82b7ecf560ae9d4195d44f702becfa
Martin Silkjaer <ms@willowpond.dk>
2025-03-01 00:53:02 +0100

    nice

--- config.py
+++ config.py
@@ -6,56 +6,56 @@ import pulumi
 from pulumi import Config
 
 # Create Config objects for each namespace
-proxmox = Config("proxmox")
-vm = Config("vm")
-k3s = Config("k3s")
-ansible = Config("ansible")
+proxmox_config_obj = Config("proxmox")
+vm_config_obj = Config("vm")
+k3s_config_obj = Config("k3s")
+ansible_config_obj = Config("ansible")
 
 # Proxmox configuration
 proxmox_config = {
-    "endpoint": pulumi.config.get("proxmox:endpoint") or "https://proxmox.example.com:8006/api2/json",
-    "username": pulumi.config.require("proxmox:username"),
-    "password": pulumi.config.require_secret("proxmox:password"),
-    "insecure": pulumi.config.get_bool("proxmox:insecure") or True,  # Skip TLS verification
-    "node": pulumi.config.get("proxmox:node") or "pve",  # Default node name
+    "endpoint": proxmox_config_obj.get("endpoint") or "https://proxmox.example.com:8006/api2/json",
+    "username": proxmox_config_obj.require("username"),
+    "password": proxmox_config_obj.require_secret("password"),
+    "insecure": proxmox_config_obj.get_bool("insecure") or True,  # Skip TLS verification
+    "node": proxmox_config_obj.get("node") or "pve",  # Default node name
 }
 
 # VM Base Configuration
 vm_base_config = {
-    "description": pulumi.config.get("vm:description") or "K3s node created with Pulumi",
-    "template": pulumi.config.require("vm:template"),  # VM template ID
-    "cores": pulumi.config.get_int("vm:cores") or 2,
-    "memory": pulumi.config.get_int("vm:memory") or 4096,  # MB
-    "disk_size": pulumi.config.get("vm:disk_size") or "20G",
-    "ssh_public_key": pulumi.config.require("vm:ssh_public_key"),
-    "ssh_private_key_path": pulumi.config.require("vm:ssh_private_key_path"),
-    "ssh_user": pulumi.config.get("vm:ssh_user") or "ubuntu",
-    "network_bridge": pulumi.config.get("vm:network_bridge") or "vmbr0",
+    "description": vm_config_obj.get("description") or "K3s node created with Pulumi",
+    "template": vm_config_obj.require("template"),  # VM template ID
+    "cores": vm_config_obj.get_int("cores") or 2,
+    "memory": vm_config_obj.get_int("memory") or 4096,  # MB
+    "disk_size": vm_config_obj.get("disk_size") or "20G",
+    "ssh_public_key": vm_config_obj.require("ssh_public_key"),
+    "ssh_private_key_path": vm_config_obj.require("ssh_private_key_path"),
+    "ssh_user": vm_config_obj.get("ssh_user") or "ubuntu",
+    "network_bridge": vm_config_obj.get("network_bridge") or "vmbr0",
 }
 
 # K3s Cluster Configuration
 k3s_cluster_config = {
     # Master nodes
-    "master_count": pulumi.config.get_int("k3s:master_count") or 1,
-    "master_name_prefix": pulumi.config.get("k3s:master_name_prefix") or "k3s-master",
-    "master_cores": pulumi.config.get_int("k3s:master_cores") or vm_base_config["cores"],
-    "master_memory": pulumi.config.get_int("k3s:master_memory") or vm_base_config["memory"],
+    "master_count": k3s_config_obj.get_int("master_count") or 1,
+    "master_name_prefix": k3s_config_obj.get("master_name_prefix") or "k3s-master",
+    "master_cores": k3s_config_obj.get_int("master_cores") or vm_base_config["cores"],
+    "master_memory": k3s_config_obj.get_int("master_memory") or vm_base_config["memory"],
     
     # Worker nodes
-    "worker_count": pulumi.config.get_int("k3s:worker_count") or 0,
-    "worker_name_prefix": pulumi.config.get("k3s:worker_name_prefix") or "k3s-worker",
-    "worker_cores": pulumi.config.get_int("k3s:worker_cores") or vm_base_config["cores"],
-    "worker_memory": pulumi.config.get_int("k3s:worker_memory") or vm_base_config["memory"],
+    "worker_count": k3s_config_obj.get_int("worker_count") or 0,
+    "worker_name_prefix": k3s_config_obj.get("worker_name_prefix") or "k3s-worker",
+    "worker_cores": k3s_config_obj.get_int("worker_cores") or vm_base_config["cores"],
+    "worker_memory": k3s_config_obj.get_int("worker_memory") or vm_base_config["memory"],
     
     # Static IP configuration (optional)
-    "use_static_ips": pulumi.config.get_bool("k3s:use_static_ips") or False,
-    "ip_network": pulumi.config.get("k3s:ip_network") or "192.168.1.0/24",
-    "ip_gateway": pulumi.config.get("k3s:ip_gateway") or "192.168.1.1",
-    "ip_start": pulumi.config.get_int("k3s:ip_start") or 100,  # e.g., 192.168.1.100, 192.168.1.101, etc.
+    "use_static_ips": k3s_config_obj.get_bool("use_static_ips") or False,
+    "ip_network": k3s_config_obj.get("ip_network") or "192.168.1.0/24",
+    "ip_gateway": k3s_config_obj.get("ip_gateway") or "192.168.1.1",
+    "ip_start": k3s_config_obj.get_int("ip_start") or 100,  # e.g., 192.168.1.100, 192.168.1.101, etc.
     
     # k3s version and settings
-    "version": pulumi.config.get("k3s:version") or "v1.29.2+k3s1",
-    "install_args": pulumi.config.get("k3s:install_args") or "",
+    "version": k3s_config_obj.get("version") or "v1.29.2+k3s1",
+    "install_args": k3s_config_obj.get("install_args") or "",
 }
 
 # Ansible Configuration
@@ -63,11 +63,11 @@ k3s_cluster_config = {
 # For example: pulumi config set ansible:system_timezone "America/New_York"
 # These values will be passed as extra_vars to the Ansible playbook
 ansible_config = {
-    "repo_url": pulumi.config.get("ansible:repo_url") or "https://github.com/techno-tim/k3s-ansible.git",
-    "repo_branch": pulumi.config.get("ansible:repo_branch") or "master",
-    "local_path": pulumi.config.get("ansible:local_path") or "k3s-ansible",
-    "use_ansible": pulumi.config.get_bool("ansible:use_ansible") or True,
-    "cache_repo": pulumi.config.get_bool("ansible:cache_repo") or True,
+    "repo_url": ansible_config_obj.get("repo_url") or "https://github.com/techno-tim/k3s-ansible.git",
+    "repo_branch": ansible_config_obj.get("repo_branch") or "master",
+    "local_path": ansible_config_obj.get("local_path") or "k3s-ansible",
+    "use_ansible": ansible_config_obj.get_bool("use_ansible") or True,
+    "cache_repo": ansible_config_obj.get_bool("cache_repo") or True,
     # Additional configuration values with 'ansible:' prefix will be passed 
     # directly to the all.yml file. See README for examples.
 } 
--- proxmox/vm.py
+++ proxmox/vm.py
@@ -8,9 +8,8 @@ from typing import Dict, Any, Optional
 
 class ProxmoxVM:
     """
-    Create and manage a Proxmox VE virtual machine.
+    Represents a Proxmox VM created from a template.
     """
-    
     def __init__(
         self,
         provider: proxmox.Provider,
@@ -28,20 +27,20 @@ class ProxmoxVM:
         dns_server: str = "8.8.8.8",
     ):
         """
-        Initialize a new ProxmoxVM.
+        Creates a new VM from a template in Proxmox.
         
         Args:
-            provider: The Proxmox provider
-            name: Name of the VM
+            provider: Pulumi Proxmox provider
+            name: VM name
             node: Proxmox node name
-            template_id: VM template ID
+            template_id: Template ID to clone from (format: pve/vmid or just vmid)
             cores: Number of CPU cores
             memory: Memory in MB
             description: VM description
             disk_size: Disk size (e.g., "20G")
             ssh_public_key: SSH public key for cloud-init
             network_bridge: Network bridge to use
-            ip_address: Static IP address (optional, uses DHCP if not provided)
+            ip_address: Static IP address in CIDR notation (e.g., "192.168.1.100/24")
             gateway: Network gateway (required if ip_address is set)
             dns_server: DNS server
         """
@@ -50,59 +49,89 @@ class ProxmoxVM:
         self.node = node
         self.template_id = template_id
         
-        # Configure VM clone from template
+        # Extract VM ID from template_id
+        if "/" in template_id:
+            vm_id = int(template_id.split("/")[1])
+        else:
+            vm_id = int(template_id)
+        
+        # Prepare VM arguments
         vm_args = {
             "node_name": node,
-            "clone": proxmox.vm.VmCloneArgs(
-                vm_id=int(template_id.split("/")[1]),
-                full=True,
-            ),
             "name": name,
-            "description": description,
-            "cpu": proxmox.vm.VmCpuArgs(
-                cores=cores,
-            ),
-            "memory": proxmox.vm.VmMemoryArgs(
-                dedicated=memory,
-            ),
-            "network_device": proxmox.vm.VmNetworkDeviceArgs(
-                bridge=network_bridge,
-                model="virtio",
-            ),
+            "clone": {
+                "vm_id": vm_id,
+                "full": True,
+            },
+            "cpu": {
+                "cores": cores,
+            },
+            "memory": {
+                "dedicated": memory,
+            },
+            "network_devices": [
+                {
+                    "bridge": network_bridge,
+                    "model": "virtio",
+                }
+            ],
+            "agent": {
+                "enabled": True,
+            },
             "on_boot": True,
         }
         
-        # Add cloud-init configuration if SSH key is provided
+        # Add description if provided
+        if description:
+            vm_args["description"] = description
+            
+        # Add initialization configuration if SSH key is provided
         if ssh_public_key:
-            cloud_init = {
-                "user": "ubuntu",  # Default user for cloud-init
-                "ssh_key": ssh_public_key,
+            init_config = {
+                "type": "nocloud",
+                "user_account": {
+                    "username": "ubuntu",  # Default user for cloud-init
+                    "keys": [ssh_public_key],
+                }
             }
             
             # Add network configuration if static IP is provided
             if ip_address and gateway:
-                cloud_init["ipconfig"] = [
-                    f"ip={ip_address}/24,gw={gateway}"
+                init_config["ip_configs"] = [
+                    {
+                        "ipv4": {
+                            "address": ip_address,
+                            "gateway": gateway
+                        }
+                    }
                 ]
-                cloud_init["nameserver"] = dns_server
                 
-            vm_args["cloud_init"] = cloud_init
+                # Add DNS configuration
+                init_config["dns"] = {
+                    "servers": [dns_server]
+                }
+                
+            vm_args["initialization"] = init_config
         
         # Create the VM
-        self.vm = proxmox.vm.Vm(
+        self.vm = proxmox.vm.VirtualMachine(
             name,
-            opts=pulumi.ResourceOptions(provider=provider),
-            **vm_args
+            **vm_args,
+            opts=pulumi.ResourceOptions(provider=provider)
         )
 
     @property
     def ip_address(self) -> pulumi.Output[str]:
-        """Get the VM IP address"""
-        return self.vm.ip_addresses.apply(lambda addresses: addresses[0] if addresses else None)
+        """
+        Returns the IP address of the VM.
+        """
+        return self.vm.ipv4_addresses.apply(lambda addresses: addresses[0] if addresses else None)
 
     @property
     def vm_id(self) -> pulumi.Output[int]:
-        """Get the VM ID"""
+        """
+        Returns the VM ID.
+        """
         return self.vm.vm_id
 
 def create_proxmox_vm(
